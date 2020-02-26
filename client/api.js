d9990789574657b07cd02aa4db87e1faab96b61f
kaewkors <kaewkor.s@gmail.com>
2020-02-27 11:38:31 +1300

    post server.js set up

--- client/api.js
+++ client/api.js
@@ -1,1 +1,10 @@
 import request from 'superagent'
+
+export function getPosts() {
+    console.log('meetup posts')
+    return request.get('/api/lost/posts')
+    .then(res => {
+        console.log(res)
+        return res.body
+    })
+}
---
+++ server/routes/posts.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const db = require('../db/db')
+
+const router = express.Router()
+
+router.use(express.json())
+
+//get.router('/')
+
+module.exports = router
