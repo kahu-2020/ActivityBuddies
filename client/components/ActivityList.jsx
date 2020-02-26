86892bbcc9941d90b377e0d016067c508af7282e
student <student@eda-bc-03.localdomain>
2020-02-27 10:44:49 +1300

    display placeholders on screen

---
+++ client/components/ActivityList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Activity from './Activity'
+
+function ActivityList () {
+    return (
+        <React.Fragment>
+            <Activity/>
+        </React.Fragment>
+    )
+}
+
+
+export default ActivityList
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { HashRouter as Router, Route } from 'react-router-dom'
+import ActivityList from './ActivityList'
 
 
 class App extends React.Component{
@@ -9,6 +10,7 @@ class App extends React.Component{
   return (
     <Router>
       <div>
+        <Route path="/" component={ActivityList} />
       </div>
     </Router>
 
