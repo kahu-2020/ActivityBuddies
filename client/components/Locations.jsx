df9134eba6fa6bdf6da8dcf86bd33bc1fe0ead11
claire-russell <russell.claireolivia@gmail.com>
2020-02-27 11:50:23 +1300

    component started

---
+++ client/components/Locations.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+// import { actionName } from '../actions/index'
+
+
+
+class Locations extends React.Component {
+    constructor(props) {
+        super(props)
+       
+        // this.handleClick = this.handleClick.bind(this);
+    }
+
+
+    componentDidMount() {
+        // this.props.dispatch(getLocations(this.props.currentActivity.id))
+
+    }
+
+
+    handleClick() {
+    //    this.props.dispatch(setLocation(this.props.locations.id))
+    }
+
+
+
+    render() {
+
+        return (
+            <div>
+                <h1>Mountain Biking</h1>
+                <h2>Locations</h2>
+            </div>
+        )
+    }
+}
+
+
+function mapStateToProps(state) {
+    return {
+       currentActivity: state.currentActivity, 
+       locations: state.locations, 
+       currentLocation: state.currentLocation
+    }
+}
+
+export default connect(mapStateToProps)(Locations)
