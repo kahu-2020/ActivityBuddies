a2fb0308c1e66cc86dcc3eea45aa2fa37322e0d9
claire-russell <russell.claireolivia@gmail.com>
2020-02-27 14:27:40 +1300

    component locations setUp

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,14 +4,17 @@ import { HashRouter as Router, Route } from 'react-router-dom'
 import ActivityList from './ActivityList'
 
 import AddPostForm from './AddPostForm'
+import Locations from './Locations'
 
 class App extends React.Component{
+ 
 
   render() {
   return (
     <Router>
       <div>
-        <Route path="/" component={ActivityList} />
+        <Route exact path="/" component={ActivityList} />
+        <Route path='/locations' component={Locations} />
         <Route path='/addpostform' component={AddPostForm} />
       </div>
     </Router>
--- client/components/Locations.jsx
+++ client/components/Locations.jsx
@@ -8,30 +8,44 @@ import { connect } from 'react-redux'
 class Locations extends React.Component {
     constructor(props) {
         super(props)
-       
+
         // this.handleClick = this.handleClick.bind(this);
     }
 
 
-    componentDidMount() {
-        // this.props.dispatch(getLocations(this.props.currentActivity.id))
+    // componentDidMount() {
+    //     this.props.dispatch(getLocations(this.props.currentActivity.id)) // gets all locations
 
-    }
+    // }
 
 
-    handleClick() {
-    //    this.props.dispatch(setLocation(this.props.locations.id))
-    }
+    // handleClick(evt) {
+    //     evt.preventDefault()
+    //    this.props.dispatch(setCurrentLocation(this.props.locations.id)) // sets the global state of currentLocation
+    // }
 
 
 
     render() {
 
         return (
-            <div>
-                <h1>Mountain Biking</h1>
-                <h2>Locations</h2>
-                
+            <div className='wrapper'>
+                <div className='WellingtonImgs'>
+                    <img src="images/makaraMap.png" alt="Makara pinpointed in Wellington map" />
+                    <img src="images/polhillMap.png" alt="Polhill pinpointed in Wellington map" />
+                    <img src="images/mtVicMap.png" alt="MtVic pinpointed in Wellington map" />
+                </div>
+                <div className='locationWrapper'>
+                    <h1>Mountain Biking</h1>
+                    <h2>Locations to choose from</h2>
+                    <div className='locationList'>
+                        {this.props.locations.map(location => {
+                            return <p>{location}</p>
+                        })}
+                    </div>
+                </div>
+
+
             </div>
         )
     }
@@ -40,9 +54,9 @@ class Locations extends React.Component {
 
 function mapStateToProps(state) {
     return {
-       currentActivity: state.currentActivity, 
-       locations: state.locations, 
-       currentLocation: state.currentLocation
+        currentActivity: state.currentActivity,
+        locations: state.locations,
+        currentLocation: state.currentLocation
     }
 }
 
