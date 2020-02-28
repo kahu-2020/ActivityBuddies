1d95394482e514f48804c0e97ca63faf87813a4f
student <student@eda-bc-03.localdomain>
2020-02-28 16:14:52 +1300

    more css

--- client/components/ActivityList.jsx
+++ client/components/ActivityList.jsx
@@ -15,10 +15,11 @@ class ActivityList extends React.Component {
     }
 
     render() {
+<a href=""></a>
         return (
             <div className='activity-container'>
                 {this.props.activities.map(activity => {
-                    return <Link className='act-btn hover-1' path='/locations'>< p   key={activity.id}>{activity.name}</p></Link>
+                    return <Link to="locations" className='act-btn hover-1' ><p className='front-links' key={activity.id}>{activity.name}</p></Link>
                 })}
             </div>
         )
--- client/components/Locations.jsx
+++ client/components/Locations.jsx
@@ -29,22 +29,21 @@ class Locations extends React.Component {
             <div className='wrapper'>
                 <h1>RIDE</h1>
                 <div className='wrapperBody'>
-                <div className='imagesWrapper'>
-                    <img src="images/makaraMap.png" className='mapImages' alt="Makara pinpointed in Wellington map" />
-                    {/* <img src="images/polhillMap.png" className='mapImages' alt="Polhill pinpointed in Wellington map" />
-                    <img src="images/mtVicMap.png" className='mapImages' alt="MtVic pinpointed in Wellington map" /> */}
-                </div>
+                    <div className='imagesWrapper'>
+                        <img src="images/makara.png" className='mapImages' alt="Makara pinpointed in Wellington map" />
 
-                <div className='locationWrapper'>
-                    
-                    <div className='locationList'>
-                        {this.props.locations.map(location => {
-                            return <Link to='/meetups' className='links'>  
-                                <button className='btn btn-outline-primary' key={location.id} onClick={() => this.handleClick(location)}>{location.name}</button>
-                            </Link>
-                        })}
                     </div>
-</div>
+
+                    <div className='locationWrapper'>
+
+                        <div className='locationList'>
+                            {this.props.locations.map(location => {
+                                return <Link to='/meetups' className='links'>
+                                    <button className='btn btn-outline-primary' key={location.id} onClick={() => this.handleClick(location)}>{location.name}</button>
+                                </Link>
+                            })}
+                        </div>
+                    </div>
                 </div>
 
 
