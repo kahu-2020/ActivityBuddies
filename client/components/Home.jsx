9325b91203779d7f5718e578126441d5cc414882
student <student@eda-bc-03.localdomain>
2020-02-28 13:08:29 +1300

    almost finished css

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -19,7 +19,7 @@ class App extends React.Component {
       <div>
           <ActivityList/>
           <p className="about-blurb">This is a shitty placeholder text that tells you sweet fuck all about this website but looks pretty while doing nothing. And it has to take up a decent portion of the page which is why im typing this sentence.
-           So anyway hows the mother? also the text shadow isnt working with this like it did with the main links so thats a problem we need to fix</p>
+</p>
       </div>
     )
   }
