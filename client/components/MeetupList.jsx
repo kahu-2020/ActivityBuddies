80342cb2ec073134c3664c6883ffd28b34c7373d
Henry-Harcourt <hnry.harcourt@gmail.com>
2020-03-01 13:09:31 +1300

    add dropdown to change skill-level in state

--- client/components/MeetupList.jsx
+++ client/components/MeetupList.jsx
@@ -9,20 +9,53 @@ class MeetupList extends React.Component {
         super(props)
 
         this.state = {
-            testPosts: [1 ,2 , 3, 4, 5, 6, 7, 8]
+            testPosts: [1, 2, 3, 4, 5, 6, 7, 8],
+            skillLevel: ""
         }
+
+        this.handleChange = this.handleChange.bind(this)
+        // this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    // handleSubmit = (e) => {
+    //     console.log(e.target.value)
+    //     e.preventDefault()
+
+    //     this.setState({
+    //         skillLevel: [e.target.value]
+    //     })
+
+    // }
+
+    handleChange = (e) => {
+        console.log(e.target.value)
+        this.setState({
+            [e.target.name]: e.target.value
+        })
     }
 
     render() {
         return (
-            <div className="meetupList"> 
+            <div className="meetupList">
                 <h2>Meetups</h2>
-                <button onClick={this.props.handleClick} className="addButton"> + </button>
                 
+                <form>
+                    <select value={this.setState.skillLevel} onChange={this.handleChange}>
+                        <option value="">--Select your skill level--</option>
+                        <option value="Beginner">Beginner</option>
+                        <option value="Intermediate">Intermediate</option>
+                        <option value="Advanced">Advanced</option>
+                        <option value="Expert">Expert</option>
+                    </select>
+                    <input type="submit" value ="submit"/>
+                </form>
+                
+                <button onClick={this.props.handleClick} className="addButton"> + </button>
+
                 <div className="cardList">
-                {this.props.posts.map( (post, i) => {
-                    return <MeetupPost key={i} currentPost={post} />
-                })}
+                    {this.props.posts.map((post, i) => {
+                        return <MeetupPost key={i} currentPost={post} />
+                    })}
                 </div>
             </div>
         )
